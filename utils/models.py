042f62b98a3884fed5e489ca37e9ff16b136436f
Gabriel Arteaga <gabriel.arteaga.1996@hotmail.com>
2023-11-22 16:49:48 +0100

    Made BatchEnsemble w/ mean and variance layer

--- utils/models.py
+++ utils/models.py
@@ -11,8 +11,7 @@ class BatchEnsemble(nn.Module):
                  ensemble_size: int,
                  input_shape: int, 
                  hidden_layers: int, 
-                 hidden_units: int, 
-                 output_shape: int):
+                 hidden_units: int):
         super().__init__()
 
         # Create the first layer
@@ -31,10 +30,41 @@ class BatchEnsemble(nn.Module):
         # Add the output layer
         layers.append(BatchLinear(ensemble_size=ensemble_size,
                                   in_features=hidden_units,
-                                  out_features=output_shape))
+                                  out_features=hidden_units))
 
         # Create the layer stack
         self.layer_stack = nn.Sequential(*layers)
 
+        # Create mean layer
+        self.mean_layer = BatchLinear(ensemble_size=ensemble_size,
+                                     in_features=hidden_units,
+                                     out_features=1)
+        # Create variance layer
+        self.var_layer = BatchLinear(ensemble_size=ensemble_size,
+                                     in_features=hidden_units,
+                                     out_features=1)
+
     def forward(self, x):
-        return self.layer_stack(x)
+        # Returns hidden units
+        z = self.layer_stack(x)
+
+        # Send the hidden units to mean and variance layer
+        mean = self.mean_layer(z)
+        # Ensure that variance is >= 0
+        var = F.softplus(self.var_layer(z))
+        return mean, var
+
+class AnchoredBatchEnsemble(nn.Module):
+    def __init__(self,
+                 ensemble_size: int,
+                 input_shape: int, 
+                 hidden_layers: int, 
+                 hidden_units: int, 
+                 output_shape: int):
+        super().__init__()
+
+        # Create the first layer
+        layers = [BatchLinear(ensemble_size=ensemble_size,
+                              in_features=input_shape,
+                              out_features=hidden_units), 
+                  nn.ReLU()]
