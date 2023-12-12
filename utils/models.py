412542a20bc03bbca7586dbcab868fbfbf6e219c
Gabriel-Arteaga <gabriel.arteaga.1996@hotmail.com>
2023-12-12 22:36:36 +0100

    Added Base NN w/ h_layers/units as hyperparaters w/ Kaiming init. linear layers.

--- utils/models.py
+++ utils/models.py
@@ -3,8 +3,7 @@ sys.path.append('..')
 import torch
 from torch import nn
 from torch.nn import functional as F
-from utils.layers import BatchLinear
-from utils.layers import AnchoredBatch
+from utils.layers import *
 # Create a BaseBatchEnsemble Class which take in layers and units as arguments
 class BatchEnsemble(nn.Module):
     def __init__(self,
@@ -150,9 +149,43 @@ class AnchoredBatchEnsemble(nn.Module):
     def forward(self, x):
         # Returns hidden units
         z = self.layer_stack(x)
-
         # Send the hidden units to mean and variance layer
         mean = self.mean_layer(z)
         # Ensure that variance is >= 0
         var = F.softplus(self.var_layer(z))
         return mean, var
+    
+
+class KaimingNN(nn.Module):
+    def __init__(self,
+                 input_shape: int, 
+                 hidden_layers: int, 
+                 hidden_units: int):
+        super().__init__()
+
+        # Create the first layer
+        layers = [Kaiming_Linear(in_features=input_shape,
+                              out_features=hidden_units,
+                              ), 
+                  nn.ReLU()]
+
+        # Add the hidden layers
+        for _ in range(hidden_layers - 1):
+            layers.append(Kaiming_Linear(in_features=hidden_units,
+                                      out_features=hidden_units))
+            layers.append(nn.ReLU())
+
+        # Add the output layer
+        layers.append(Kaiming_Linear(in_features=hidden_units,
+                                  out_features=hidden_units))
+
+        # Create the layer stack
+        self.layer_stack = nn.Sequential(*layers)
+
+        # Create mean layer
+        self.mean_layer = Kaiming_Linear(in_features=hidden_units,
+                                     out_features=1)
+        # Create variance layer
+        self.var_layer = Kaiming_Linear(
+                                     in_features=hidden_units,
+                                     out_features=1)
